acd851ca1d5b32117753608d352722a8e8131389
Vipul Gaurav <30434974+vgaurav3011@users.noreply.github.com>
2020-04-16 15:45:14 +0530

    Create rectangle.py

---
+++ Vipul/Day 2/rectangle.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.__length = length
+        self.__width = width
+
+    def area(self):
+        return (self.__length * self.__width)
+
+    def perimeter(self):
+        return (2 * (self.__length + self.__width))
+obj = Rectangle(4, 5)
+print("Area is", obj.area())
+print("Perimeter is", obj.perimeter())
